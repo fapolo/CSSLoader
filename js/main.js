42191040b991c1da9449054f9f0cddbc5abf2a8b
Fabricio Polo <fabricio.polo@gmail.com>
2019-05-30 23:11:49 -0300

    loaders put together

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-const loader = document.querySelector('.loader');
+const loader1 = document.querySelector('.loader1');
+const loader2 = document.querySelector('.loader2');
+const loader3 = document.querySelector('.loader3');
+const loaders = [loader1, loader2, loader3];
 const main = document.querySelector('.main');
 
 function init() {
     setTimeout(() => {
-        loader.style.opacity = 0;
-        loader.style.display = 'none';
-
+        for (var i = 0; i < 3; i++) {
+            loaders[i].style.opacity = 0;
+            loaders[i].style.display = 'none';
+        };
         main.style.display = 'block';
         setTimeout(() => (main.style.opacity = 1), 50);
     }, 4000);
